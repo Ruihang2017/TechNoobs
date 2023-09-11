ab50dd291083aad5bccbcbfd6e7333e5fa4f1584
Ruihang2017 <94610820+Ruihang2017@users.noreply.github.com>
2023-09-11 20:35:36 +1000

    blog PUT

--- controllers/blogRoutes.js
+++ controllers/blogRoutes.js
@@ -2,53 +2,6 @@ const router = require("express").Router();
 const { User, Comment, Blog } = require("../models");
 // const withAuth = require("../utils/auth");
 
-// // // Get information for all tasks
-// router.get("/", withAuth, async (req, res) => {
-//   try {
-//     const tasksData = await Task.findAll({
-//       include: [
-//         {
-//           model: TaskStatus,
-//         },
-//         { model: Employee, through: EmployeeTask, as: "task_employees" },
-//       ],
-//     });
-//     if (!tasksData) {
-//       res.status(404).json({ message: "No tasks found!" });
-//       return;
-//     }
-
-//     res.status(200).json(tasksData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// // Get information for one tasks
-// router.get("/:id", withAuth, async (req, res) => {
-//   try {
-//     const taskData = await Task.findOne({
-//       where: { id: req.params.id },
-//       include: [
-//         {
-//           model: TaskStatus,
-//         },
-//         { model: Employee, through: EmployeeTask, as: "task_employees" },
-//       ],
-//     });
-
-//     if (!taskData) {
-//       res
-//         .status(404)
-//         .json({ message: `No tasks found with this id: ${req.params.id}!` });
-//       return;
-//     }
-//     res.status(200).json(taskData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 // // Create task
 router.post("/", async (req, res) => {
   try {
@@ -64,6 +17,22 @@ router.post("/", async (req, res) => {
     });
     console.log(`blog created ${blog}`);
     res.status(201).json({ message: "blog created", blog });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// // Update a task
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedRows = await Blog.update(req.body, {
+      where: { id: req.params.id },
+    });
+    if (updatedRows[0] === 0) {
+      res.status(404).json({ message: "No task found with this id!" });
+      return;
+    }
+    res.status(200).json(updatedRows);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -86,97 +55,5 @@ router.delete("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-// // Update a task
-// router.patch("/:id/status", withAuth, async (req, res) => {
-//   try {
-//     const statusData = await TaskStatus.findOne({
-//       where: {
-//         status_name: req.body.status_name,
-//       },
-//     });
-
-//     if (!statusData) {
-//       return res.status(404).json({ message: "Status not found!" });
-//     }
-
-//     const statusId = statusData.id;
-
-//     // Update the task with the new status ID
-//     const [numberOfAffectedRows] = await Task.update(
-//       { status_id: statusId },
-//       {
-//         where: {
-//           id: req.params.id,
-//         },
-//       },
-//     );
-
-//     if (numberOfAffectedRows === 0) {
-//       return res.status(404).json({ message: "Task not found!" });
-//     }
-
-//     res.json({ message: "Task status updated successfully." });
-//   } catch (error) {
-//     res
-//       .status(500)
-//       .json({ message: "Error updating task status.", error: error.message });
-//   }
-// });
-
-// // Update a task
-// router.put("/:id", withAuth, async (req, res) => {
-//   try {
-//     const updatedRows = await Task.update(req.body, {
-//       where: { id: req.params.id },
-//     });
-//     if (updatedRows[0] === 0) {
-//       res.status(404).json({ message: "No task found with this id!" });
-//       return;
-//     }
-
-//     if (req.body.employeeIds) {
-//       const newTaskEmployeeArr = req.body.employeeIds.map((employeeId) => {
-//         return {
-//           employee_id: employeeId.employee_id,
-//           task_id: req.params.id,
-//         };
-//       });
-
-//       const taskEmployees = await EmployeeTask.findAll({
-//         where: { task_id: req.params.id },
-//       });
-//       const taskEmployeesID = taskEmployees.map(
-//         (employee_task) => employee_task.id,
-//       );
-
-//       await EmployeeTask.destroy({ where: { id: taskEmployeesID } });
-
-//       res.status(200).json(await EmployeeTask.bulkCreate(newTaskEmployeeArr));
-//     } else {
-//       res.status(200).json({ message: "Task updated successfully!" });
-//     }
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-// // Delete a task
-// router.delete("/:id", withAuth, async (req, res) => {
-//   try {
-//     const taskData = await Task.destroy({
-//       where: { id: req.params.id },
-//     });
-//     if (!taskData) {
-//       res
-//         .status(404)
-//         .json({ message: `No tasks found with this id: ${req.params.id}!` });
-//       return;
-//     }
-//     res.status(200).json(taskData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
--- public/js/blog.js
+++ public/js/blog.js
@@ -13,9 +13,12 @@ if (
 }
 
 const deleteButton = document.getElementById("deleteButton");
+const saveButton = document.getElementById("saveButton");
+const blogContentTextarea = document.getElementById("blogContentTextarea");
 
+// deleteButton event listener
 deleteButton.addEventListener("click", async (event) => {
-  console.log("click");
+  console.log("deleteButton click");
   try {
     const id = window.location.href.split("/").pop();
     const urlBlog = url + `api/blog/${id}`;
@@ -31,5 +34,50 @@ deleteButton.addEventListener("click", async (event) => {
   } catch (error) {
     console.error("Error:", error);
     // return { success: false, error: error.message };
+  }
+});
+
+// saveButton event listener
+saveButton.addEventListener("click", async (event) => {
+  console.log("saveButton click");
+  try {
+    const id = window.location.href.split("/").pop();
+    const urlBlog = url + `api/blog/${id}`;
+
+    // Create the request headers
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    if (!blogContentTextarea) {
+      console.error("blogContentTextarea is null");
+    }
+
+    const reqData = {
+      blog_content: blogContentTextarea.value,
+    };
+    console.log(urlBlog);
+
+    // // Create the request options
+    const requestOptions = {
+      method: "PUT",
+      headers: headers,
+      body: JSON.stringify(reqData),
+    };
+
+    // // Send the POST request
+    fetch(urlBlog, requestOptions)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        // Handle any errors that occurred during the fetch
+        console.error("Error:", error);
+      });
+  } catch (error) {
+    console.error("Error:", error);
   }
 });
